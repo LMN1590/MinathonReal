1bfffdb1b963c5df171d17144f6957bcd5c2072f
unknown <minhnghiale1590@gmail.com>
2022-04-24 11:26:10 +0700

    Functionality

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,32 @@
 function updatePlaceHolder(title){
     document.getElementById('realTitle').textContent=title;
+    document.getElementById('id-desc').value=dict[title];
+    console.log(dict[title]);
 }
-var arr=document.getElementsByClassName('card')
-arr[0].onclick=updatePlaceHolder(this.children[0].innerHTML)
+function init(){
+    var arr=document.getElementsByClassName('card');
+    for(const a of arr){
+        a.setAttribute("onclick","updatePlaceHolder(this.children[0].innerHTML)");
+        
+    }
+    dict['Plan']="Aloha";
+    dict['Budget']="Hello";
+    dict['Goal']="Hello";
+    dict['Participants']="Hello";
+    dict['Activites']="Hello";
+    dict['Location']="Hello"; 
+    document.getElementById("id-desc").addEventListener("keypress", submitOnEnter);
+}
+var dict={};
+function submitOnEnter(event){
+    if(event.which === 13 && !event.shiftKey){
+        var title=document.getElementById('realTitle').textContent;
+        dict[title]=this.value;
+        this.textContent="";
+        console.log(dict[title]);
+    }
+}
+
+
+
+
