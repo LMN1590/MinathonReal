0ce721abc4dfe961ea0eb20ff43dde973676740a
unknown <minhnghiale1590@gmail.com>
2022-04-24 11:46:11 +0700

    fixed desc

--- JS/script.js
+++ JS/script.js
@@ -9,12 +9,12 @@ function init(){
         a.setAttribute("onclick","updatePlaceHolder(this.children[0].innerHTML)");
         
     }
-    dict['Plan']="Aloha";
-    dict['Budget']="Hello";
-    dict['Goal']="Hello";
-    dict['Participants']="Hello";
-    dict['Activites']="Hello";
-    dict['Location']="Hello"; 
+    dict['Plan']="Vacation to Hawaii \n2 day one night \nwith an ass";
+    dict['Budget']="$1000 per person x 50 participants = $50000";
+    dict['Goal']="To strengthen the bonds between employees";
+    dict['Participants']="50 people planned";
+    dict['Activites']="8h00: Breakfast\n12h00: Lunch\n18h00: Dinner";
+    dict['Location']="Definitely-real Hotel"; 
     document.getElementById("id-desc").addEventListener("keypress", submitOnEnter);
 }
 var dict={};
